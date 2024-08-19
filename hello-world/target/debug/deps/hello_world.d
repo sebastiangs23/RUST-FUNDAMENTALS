d262e1eef9580560842f66409e897db68e94ed84
sebastiangs23 <sebastiangs2309@gmail.com>
2024-08-19 17:23:13 -0500

    feat(Firts hello-world in Rust):

---
+++ hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\Sebastian\Desktop\Development\rust_fundamentals\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\Sebastian\Desktop\Development\rust_fundamentals\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+C:\Users\Sebastian\Desktop\Development\rust_fundamentals\hello-world\target\debug\hello-world.exe: C:\Users\Sebastian\Desktop\Development\rust_fundamentals\hello-world\src\main.rs
