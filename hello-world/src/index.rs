57718c0796e74f36ef6b5b22be3eae62dfc5c41c
sebastiangs23 <sebastiangs2309@gmail.com>
2024-08-20 16:32:27 -0500

    feat(learning about conditionals): :sparkles:

--- hello-world/src/index.rs
+++ hello-world/src/index.rs
@@ -9,4 +9,12 @@ fn main() {
 fn aca(){
     let lastname : &str = "Gomez";
     print!("{lastname}");
+
+    let numero : i64 = 100;
+
+    if numero < 90 {
+        print!("entro en la condicional:  {lastname}")
+    }else {
+        print!("No entro en la condicional")
+    }
 }
